fe2992024cb65850af7fac17bc2213f2392c0494
Aren Siekmeier <aren.siekmeier@gmail.com>
2021-03-03 20:21:12 +0900

    actually "inherit" LegacyFairseqTask dictionary loading, but for TupleDictionary
    since the inherited methods are stuck statically referring to Dictionary and not some
    Dictionary class specific to the inheriting task, I need to reimplement
    
    instead of that
    since {load,build}_dictionary are class methods, it'd be nice to use a class variable
    to set the dict type to TupleDictionary, and call that constructor instead (and load method)
    (build's add_file_to_dictionary is a static method that doesn't need inheritance or reimplementation)
    (it would use TupleDictionary's reimplemented add_symbol)

--- tagged_translation.py
+++ tagged_translation.py
@@ -1,3 +1,4 @@
+from fairseq.data import Dictionary
 from fairseq.tasks import register_task
 from fairseq.tasks.translation import TranslationTask
 
@@ -10,5 +11,34 @@ class TaggedTranslationTask(TranslationTask):
     pass
 
   @classmethod
-  def load_dictionary(cls, *paths):
-    return TupleDictionary(*[super().load_dictionary(path) for path in paths])
+  def load_dictionary(cls, filename):
+    """Load the dictionary from the filename
+
+    Args:
+      filename (str): the filename
+    """
+    return TupleDictionary.load(filename)
+
+  @classmethod
+  def build_dictionary(
+    cls, filenames, workers=1, threshold=-1, nwords=-1, padding_factor=8
+  ):
+    """Build the dictionary
+
+    Args:
+      filenames (list): list of filenames
+      workers (int): number of concurrent workers
+      threshold (int): defines the minimum word count
+      nwords (int): defines the total number of words in the final dictionary,
+        including special symbols
+      padding_factor (int): can be used to pad the dictionary size to be a
+        multiple of 8, which is important on some hardware (e.g., Nvidia
+        Tensor Cores).
+            """
+    d = TupleDictionary()
+    for filename in filenames:
+      Dictionary.add_file_to_dictionary(
+        filename, d, tokenizer.tokenize_line, workers
+        )
+      d.finalize(threshold=threshold, nwords=nwords, padding_factor=padding_factor)
+      return d
