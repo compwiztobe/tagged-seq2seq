52695e2959f2609b45985ad1f8017f96f8952cba
Aren Siekmeier <aren.siekmeier@gmail.com>
2021-03-17 00:26:12 +0900

    first step in modifying validation code
    compute bleu correctly for just the token sequence
    and lay the groundwork for extending the valid_step and reduce_metrics functions
    to log F1 score on tags as well
    
    haven't verified that the bleu score makes sense just yet
    (and how could it if the translations themselves are still garbage)
    but at least the validation step successfully runs and saves checkpoints

--- tagged_translation.py
+++ tagged_translation.py
@@ -1,12 +1,15 @@
 import os
+import logging
 
-from fairseq import tokenizer
+from fairseq import tokenizer, utils
 from fairseq.data import Dictionary
 from fairseq.file_io import PathManager
 from fairseq.tasks import register_task
 from fairseq.tasks.translation import TranslationTask
 
 from .tuple_dictionary import TupleDictionary
+
+logger = logging.getLogger(__name__)
 
 @register_task('tagged_translation')
 class TaggedTranslationTask(TranslationTask):
@@ -55,3 +58,58 @@ class TaggedTranslationTask(TranslationTask):
       )
     d.finalize(threshold=threshold, nwords=nwords, padding_factor=padding_factor)
     return d
+
+  def valid_step(self, sample, model, criterion):
+    loss, sample_size, logging_output = super().valid_step(sample, model, criterion)
+    # if self.args.eval_F1:
+    #   F1 = self.compute_F1(...) # this has a problem, already the hypothesis is generated
+    #                             # in super().valid_step() -> _inference_with_bleu
+    #                             # no need redoing that...
+    #   logging_output['???'] = ???
+    return loss, sample_size, logging_output
+
+  def reduce_metrics(self, logging_outputs, criterion):
+    super().reduce_metrics(logging_outputs, criterion)
+    # if self.args.eval_F1:
+    #   # add up F1 metrics
+    #   pass
+
+  def _inference_with_bleu(self, generator, sample, model):
+    import sacrebleu
+
+    def decode(toks, escape_unk=False):
+      s = self.tgt_dict.string(
+        toks.int().cpu(),
+        self.args.eval_bleu_remove_bpe,
+          # The default unknown string in fairseq is `<unk>`, but
+          # this is tokenized by sacrebleu as `< unk >`, inflating
+          # BLEU scores. Instead, we use a somewhat more verbose
+          # alternative that is unlikely to appear in the real
+          # reference, but doesn't get split into multiple tokens.
+          unk_string=("UNKNOWNTOKENINREF" if escape_unk else "UNKNOWNTOKENINHYP"),
+          )
+      if self.tokenizer:
+        s = self.tokenizer.decode(s)
+      return s
+
+    gen_out = self.inference_step(generator, [model], sample, prefix_tokens=None)
+    hyps, refs = [], []
+    for i in range(len(gen_out)):
+      hyps.append(decode(gen_out[i][0]["tokens"]))
+      refs.append(
+        decode(
+          utils.strip_pad(sample["target"][i], self.tgt_dict.pad()),
+            escape_unk=True,  # don't count <unk> as matches to the hypo
+            )
+        )
+    if self.args.eval_bleu_print_samples:
+      logger.info("example hypothesis: " + hyps[0])
+      logger.info("example reference: " + refs[0])
+
+    hyps = [' '.join(pair.split(self.tgt_dict.sep)[0] for pair in hyp.split()) for hyp in hyps]
+    refs = [' '.join(pair.split(self.tgt_dict.sep)[0] for pair in ref.split()) for ref in refs]
+
+    if self.args.eval_tokenized_bleu:
+      return sacrebleu.corpus_bleu(hyps, [refs], tokenize="none")
+    else:
+      return sacrebleu.corpus_bleu(hyps, [refs])
