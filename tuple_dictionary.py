fix a critical error with index tuple computation
there was never any circumstance that required detecting a special symbol index
those are already handled before the call to compute_index
and compute_index can't properly handle those cases when there is only one factor
this is likely related to my notags and srconly bugs
and provably the cause of bugs with vanilla models on notags
(this was the reason for corruption of some sentences in the valid split)

tuple_dictionary.py
@@ -163,14 +163,10 @@ class TupleDictionary(Dictionary):
       return torch.stack(factored_indices).T
 
   def compute_index(self, indices):
-    indices = tuple(indices) # allowing generator expression in function call
-    if indices[0] < self.nspecial and all(index == -1 for index in indices[1:]):
-      return indices[0] # just in case we get sent a special symbol index tuple
-    else:
-      return self.nspecial + sum(
-        prod(self.factors[i+1:])*index
-        for i, index in enumerate(indices)
-      )
+    return self.nspecial + sum(
+      prod(self.factors[i+1:])*index
+      for i, index in enumerate(indices)
+    )
 
   #####
 