7dd593d33c1ae051afea69ef681c6ee179f32237
Aren Siekmeier <aren.siekmeier@gmail.com>
2021-06-03 16:11:06 +0900

    cleanup tag.py

--- tag.py
+++ tag.py
@@ -3,9 +3,10 @@ from flair.data import Sentence
 
 import sys
 import datetime
-import logging
 
-def tag_sentences(tagger, sentences, progress=False, batch_size=625):
+BATCH_SIZE=625
+
+def tag_sentences(tagger, sentences, progress=False, batch_size=BATCH_SIZE):
   def tag_batch(batch):
     tagger.predict(batch, mini_batch_size=batch_size)
     return batch
@@ -32,24 +33,21 @@ def is_NE(token):
   #  return token.tags['ner'].value != 'O'
   return False
 
-def main(args):
+def main(model_file, sentences, separator, batch_size=BATCH_SIZE, ner_stats=False, progress=False):
   tagger = SequenceTagger.load(args.model_file)
 
   token_count = 0
   sentence_count = 0
   NE_count = 0
 
-  sentences = (Sentence(line.strip().split()) for line in sys.stdin)
-
-  for tagged_sentence in tag_sentences(tagger, sentences, progress=args.progress, batch_size=args.batch_size):
-    if args.print:
-      yield ' '.join(t.text + args.separator + t.annotation_layers[tagger.tag_type][0].value for t in tagged_sentence.tokens)
-    if args.ner_stats:
+  for tagged_sentence in tag_sentences(tagger, sentences, progress=progress, batch_size=batch_size):
+    yield ' '.join(t.text + separator + t.annotation_layers[tagger.tag_type][0].value for t in tagged_sentence.tokens)
+    if ner_stats:
       token_count += len(tagged_sentence.tokens)
       sentence_count += 1
       NE_count += sum(is_NE(token) for token in tagged_sentence.tokens)
 
-  if args.ner_stats:
+  if ner_stats:
     return {
       'sentence_count': sentence_count,
       'token_count': token_count,
@@ -58,10 +56,11 @@ def main(args):
 
 
 import argparse
+import logging
 parser = argparse.ArgumentParser("Load a Flair NER model and use it to tag line-wise sentence data from stdin")
 parser.add_argument("-m", "--model-file", required=True,
                    help="path to Flair model file (e.g. final-model.pt)")
-parser.add_argument("--batch-size", default=625, type=int,
+parser.add_argument("--batch-size", default=BATCH_SIZE, type=int,
                     help="tagging batch size, in sentences (to optimize GPU mem use per model and dataset)")
 parser.add_argument("--ner-stats", action='store_true',# action=argparse.BooleanOptionalAction, # python>=3.9 only
                     help="print NER tag statistics to stderr upon completion") # - if used with --print-tagged, stats go to stderr, unless another file is specified for tag outputs")
@@ -71,8 +70,6 @@ parser.add_argument("--separator", default="<&&&>",
                     help="separator with which to join tokens and tags (to be parsed into a tuple by fairseq")
 parser.add_argument("--no-progress", dest='progress', default=True, action='store_false',
                     help="disable progress msgs on stderr")
-# parser.add_argument("--print-tags", metavar="TAG_FILE", default=False, nargs='?', const=True,
-#                     help="print NER tags to file, or stdout if unspecified, in .bio format (Flair input format)")
 
 def print_StopIterator(g, file=None):
   stop = yield from g
@@ -83,6 +80,20 @@ if __name__ == "__main__":
   logging.getLogger('flair').handlers[0].stream = sys.stderr
 
   args = parser.parse_args()
-  output = print_StopIterator(main(parser.parse_args()), file=sys.stderr)
-  for line in output:
-    print(line)
+  sentences = (line.strip().split() for line in sys.stdin)
+  output = print_StopIterator(
+    main(
+      args.model_file,
+      sentences,
+      args.separator,
+      batch_size=args.batch_size,
+      ner_stats=args.ner_stats,
+      progress=args.progress
+    ), file=sys.stderr
+  )
+  if args.print:
+    for line in output:
+      print(line)
+  else:
+    for _ in output:
+      pass
